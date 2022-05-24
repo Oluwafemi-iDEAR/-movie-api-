d2e04477da80c5390c4821c27155fe789fccfdd8
Oluwafemi-iDEAR <ifesanmioluwafemi@gmail.com>
2022-05-24 16:37:46 +0100

    Responsive update

--- src/Components/MyMovies.js
+++ src/Components/MyMovies.js
@@ -8,8 +8,8 @@ const MyMovies = (props) => {
                 <div className="justify-content-start m-3 d-md-flex d-sm-block">
                 <img src={movie.Poster} alt='movies'></img>
                  <div className="px-3 d-block">
-                 <h2>Year Released: {movie.Title}</h2>
-                 <h6>Year Released: {movie.Year}</h6>
+                 <h2><b>Title:</b> {movie.Title}</h2>
+                 <h6><b>Year Released:</b> {movie.Year}</h6>
                  </div>
             </div>
             </div>
