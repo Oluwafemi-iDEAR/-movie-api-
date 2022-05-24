5b2ece028330a59256a044ec1b1ad834ac0a97b9
Oluwafemi-iDEAR <ifesanmioluwafemi@gmail.com>
2022-05-24 15:56:49 +0100

    Responsive update

--- src/Components/MyMovies.js
+++ src/Components/MyMovies.js
@@ -4,13 +4,16 @@ const MyMovies = (props) => {
     return(
         <>
             {props.movies.map((movie, index)=> 
-            <div className="justify-content-start m-3 d-md-flex d-sm-block">
+            <div className="container">
+                <div className="justify-content-start m-3 d-md-flex d-sm-block">
                 <img src={movie.Poster} alt='movies'></img>
                  <div className="px-3 d-block">
                  <h2>Year Released: {movie.Title}</h2>
                  <h6>Year Released: {movie.Year}</h6>
                  </div>
-            </div>)}
+            </div>
+            </div>
+            )}
         </>
     );
 };
